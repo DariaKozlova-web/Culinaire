f71ab378fdff6170fa645a02790eb921ce080897
Daria Kozlova <kozloffa.daria@gmail.com>
2026-01-29 18:17:19 +0100

    Categories section added

--- frontend/src/components/Categories.tsx
+++ frontend/src/components/Categories.tsx
@@ -1,5 +1,25 @@
-function Categories() {
-  return <>Categories component</>;
-}
+import CategoryCard from "./CategoryCard";
+import { categoriesMock } from "./categories.mock";
+
+const Categories = () => {
+  return (
+    <section className="py-16">
+      <div className="mx-auto max-w-7xl px-4 md:px-8">
+        <h2 className="mb-10 text-3xl font-bold text-center">
+          Categories
+        </h2>
+
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          {categoriesMock.map((category) => (
+            <CategoryCard
+              key={category.id}
+              category={category}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Categories;
---
+++ frontend/src/components/CategoryCard.tsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router";
+import type { Category } from "../types/category";
+
+interface Props {
+  category: Category;
+}
+
+const CategoryCard = ({ category }: Props) => {
+  return (
+    <NavLink
+      to={`/recipes?category=${category.id}`}
+      className="
+        group relative block h-80 w-65 overflow-hidden rounded-xl
+      "
+    >
+      {/* Image */}
+      <img
+        src={category.image}
+        alt={category.title}
+        className="
+          h-full w-full object-cover
+          transition-transform duration-500
+          group-hover:scale-105
+        "
+      />
+
+      {/* Overlay */}
+      <div
+        className="
+          absolute inset-0
+          bg-[var(--bg-overlay-card)]
+          transition-opacity
+          group-hover:opacity-70
+        "
+      />
+
+      {/* Title */}
+      <div className="absolute inset-0 flex items-center justify-center p-6">
+        <h3 className="text-xl font-semibold text-[var(--text-title)]">
+          {category.title}
+        </h3>
+      </div>
+    </NavLink>
+  );
+};
+
+export default CategoryCard;
